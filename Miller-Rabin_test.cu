aaf77714146c01f2a673e54b11a9551eb1e98003
Darrin Weng <westcoasthockey2@gmail.com>
2011-06-07 23:04:49 -0700

    Got rid of random number generation only tests canidates 2, 7, 61 Runs very fast now Still has a upper limit to how many numbers can be tested not sure what the exact limit it

--- Miller-Rabin_test.cu
+++ Miller-Rabin_test.cu
@@ -6,15 +6,11 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <curand_kernel.h>
 
 #include "Miller_Rabin_Test.h"
 
-#define GRID_SIZE 100 //DONT MAKE TOO BIG WILL KILL MACHINES
-#define BLOCK_SIZE 512
-#define THREADS_PER_NUM 32
-#define KERNEL_SIZE  ((BLOCK_SIZE / THREADS_PER_NUM) * GRID_SIZE)
-
+#define GRID_SIZE 500 //DONT MAKE TOO BIG WILL KILL MACHINES
+#define BLOCK_SIZE 256
 #define CUDA_ERROR()  printf("%s\n", cudaGetErrorString(cudaGetLastError()));
 
 __device__ uint32_t modular_exponent_32(uint32_t base, uint32_t power, uint32_t modulus) 
@@ -36,27 +32,43 @@ __device__ uint32_t modular_exponent_32(uint32_t base, uint32_t power, uint32_t 
     return (uint32_t) result; /* Will not truncate since modulus is a uint32_t */
 }
 
-__global__ void setup_kernel ( curandState *state , int seed)
+__global__ void Miller_Rabin_Kernal(Test_Result *results, int num_results)
 {
-    int id = threadIdx.x + blockIdx.x * BLOCK_SIZE;
+    int index = threadIdx.x + blockIdx.x * BLOCK_SIZE;
 
-    curand_init (seed, id, 0, &state[id]) ;
-}
+    if(index >= num_results)
+        return;
 
-__global__ void Miller_Rabin_Kernal(Test_Result *results, curandState *state)
-{
-    int index = (threadIdx.x / THREADS_PER_NUM) + blockIdx.x * (BLOCK_SIZE / THREADS_PER_NUM);
     uint32_t test_num = results[index].num;
-    
-    //mod random number so that a < n
-    uint32_t a = 0;
-    while(a < 1 || a > test_num - 1)
-        a = curand(&state[threadIdx.x]) % test_num;
+    uint32_t a;
 
+    if(test_num % 2 == 0) //Even number so cannot be prime
+    {
+        results[index].passed = 1;
+        return;
+    }
+
+    switch(threadIdx.y)
+    {
+        case 0:
+            a = 2;
+        break;
+        case 1:
+            a = 7;
+        break;
+        case 2:
+            a = 61;
+        break;
+    }
+
+    //make sure a is in range
+    if( a >= test_num)
+        return;
+    
     //do test
     uint32_t a_to_power, s, d, i;
 
-    //16-bit compute s and d
+    //compute s and d
     s = 0;
     d = test_num - 1;
 
@@ -66,17 +78,10 @@ __global__ void Miller_Rabin_Kernal(Test_Result *results, curandState *state)
         s++;
     }
     
-    if(s == 0) //Even number so cannot be prime
-    {
-        results[index].passed = 1;
-        return;
-    }
-
     a_to_power = modular_exponent_32(a, d, test_num);
-    
+
     if (a_to_power == 1)
     {
-        //printf("Thread #%d %u Return 1\n", threadIdx.x, test_num);
         return;
     }
 
@@ -84,7 +89,6 @@ __global__ void Miller_Rabin_Kernal(Test_Result *results, curandState *state)
     {
         if (a_to_power == test_num - 1)
         {
-            //printf("Thread #%d %u Return 2\n", threadIdx.x, test_num);
             return;
         }
 
@@ -93,52 +97,38 @@ __global__ void Miller_Rabin_Kernal(Test_Result *results, curandState *state)
 
     if (a_to_power == test_num - 1)
     {
-        //printf("Thread #%d %u Return 3\n", threadIdx.x, test_num);
         return;
     }
     
-    //printf("Thread #%d %u Return NOT\n", threadIdx.x, test_num);
     results[index].passed = 1;
 }
 
 void run_kernel(Test_Result *results, int num_results)
 {
     printf("Running CUDA\n");
-    curandState *dev_rand_state;
     Test_Result *dev_results;
     
-    //Allocate mem for RNG states, numbers to be tested, and results
-    cudaMalloc((void **) &dev_rand_state, sizeof(curandState) * BLOCK_SIZE * GRID_SIZE);
     cudaMalloc((void **) &dev_results, sizeof(Test_Result) * num_results);
         
     //set up grid and blocksize
-    dim3 dimBlock(BLOCK_SIZE, 1);
+    dim3 dimBlock(BLOCK_SIZE, 3);
     dim3 dimGrid(GRID_SIZE, 1);
-    
-    //seed RND with cpu rand
-    srand(time(NULL));
-    int seed = rand();
-    
-    //Init RNG states and transfer data to GPU
-    setup_kernel<<<dimGrid, dimBlock>>>(dev_rand_state, seed);
     
     cudaMemcpy(dev_results, results, sizeof(Test_Result) * num_results, cudaMemcpyHostToDevice);
     
     int done = 0;
     while(done < num_results)
-    {        
-         //Run Tests
-        Miller_Rabin_Kernal<<<dimGrid, dimBlock>>>(dev_results + done, dev_rand_state);
-        
-        done += KERNEL_SIZE;
+    {
+        Miller_Rabin_Kernal<<<dimGrid, dimBlock>>>(dev_results + done, num_results);
+        done += GRID_SIZE * BLOCK_SIZE;
     }
-   
+
+    CUDA_ERROR();
     //Transfer results back to cpu
     cudaMemcpy(results, dev_results, sizeof(Test_Result) * num_results, cudaMemcpyDeviceToHost);
             
     //Clean up memory
     cudaFree(dev_results);
-    cudaFree(dev_rand_state);
 }
 
 uint32_t serial_modular_exponent_32(uint32_t base, uint32_t power, uint32_t modulus) 
@@ -153,7 +143,8 @@ uint32_t serial_modular_exponent_32(uint32_t base, uint32_t power, uint32_t modu
         }
         
         power >>= 1;
-        base = (base * base) % modulus;
+        uint64_t temp = (uint64_t) base * base;
+        base = temp % modulus;
     }
     
     return (uint32_t) result; /* Will not truncate since modulus is a uint32_t */
@@ -195,7 +186,6 @@ void Miller_Rabin_Serial(Test_Result *results, int num_results)
             
             if (a_to_power == 1)
             {
-                //printf("Thread #%d %u Return 1\n", j, test_num);
                 continue;
             }
 
@@ -203,7 +193,6 @@ void Miller_Rabin_Serial(Test_Result *results, int num_results)
             {
                 if (a_to_power == test_num - 1)
                 {
-                    //printf("Thread #%d %u Return 2\n", j, test_num);
                     continue;
                 }
 
@@ -212,11 +201,9 @@ void Miller_Rabin_Serial(Test_Result *results, int num_results)
 
             if (a_to_power == test_num - 1)
             {
-                //printf("Thread #%d %u Return 3\n", j, test_num);
                 continue;
             }
             
-            //printf("Thread #%d %u Return NOT\n", j, test_num);
             results[index].passed = 1;
         }      
     }
@@ -251,7 +238,6 @@ int main(int argc, char **argv)
     //Print results
     for(int i = 0; i < num_results; i++)
     {  
-        //printf("%d %d\n", results[i].num, results[i].passed);      
         if(results[i].passed == PASSED)
             printf("%d\n", results[i].num);      
     }
